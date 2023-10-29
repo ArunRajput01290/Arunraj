1a1769f8ef06f8c409caf6eca8b6922e39b7ecaa
Arun Rajput <52158018+ArunRajput01290@users.noreply.github.com>
2023-10-29 13:02:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,31 @@ form.addEventListener('submit', e => {
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
             msg.innerHTML = "Message sent successfully"
-            setTimeout(function(){
+            setTimeout(function () {
                 msg.innerHTML = ""
-            },6000)
+            }, 6000)
             form.reset()
         })
         .catch(error => console.error('Error!', error.message))
 })
+
+
+
+function toggleProjects() {
+    var hiddenProjects = document.querySelectorAll('.hidden');
+    var viewMoreButton = document.getElementById('view-more-button');
+    var viewLessButton = document.getElementById('view-less-button');
+
+    for (var i = 0; i < hiddenProjects.length; i++) {
+        if (hiddenProjects[i].style.display === 'none' || hiddenProjects[i].style.display === '') {
+            hiddenProjects[i].style.display = 'block';
+            viewMoreButton.style.display = 'none';
+            viewLessButton.style.display = 'block';
+        } else {
+            hiddenProjects[i].style.display = 'none';
+            viewMoreButton.style.display = 'block';
+            viewLessButton.style.display = 'none';
+        }
+    }
+}
+
